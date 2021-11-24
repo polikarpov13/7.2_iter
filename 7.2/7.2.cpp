d21cb950c397fa62a91f061b9d3151a513af9a6f
{Vlad} <{vladpolikarpovvl@gmail.com}>
2021-11-24 17:15:20 +0200

    first commit

---
+++ 7.2/7.2.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+
+using namespace std;
+
+void Create(int** array, const int k, const int n, const int Low, const int High);
+void Print(int** array, const int k, const int n);
+int min_elements_sum(int** array, const int k, const int n);
+
+void Create(int** array, const int k, const int n, const int Low, const int High)
+{
+    for (int i = 0; i < k; i++)
+        for (int j = 0; j < n; j++)
+            array[i][j] = Low + rand() % (High - Low + 1);
+}
+
+void Print(int** array, const int k, const int n)
+{
+    cout << endl;
+    for (int i = 0; i < k; i++)
+    {
+        for (int j = 0; j < n; j++)
+            cout << setw(4) << array[i][j];
+        cout << endl;
+    }
+    cout << endl;
+}
+
+
+int min_elements_sum(int** array, const int rows, const int cols)
+{
+    int sum = 0;
+    int min;
+
+    for (int i = 0; i < cols; i++)
+    {
+        if (i % 2 == 0) {
+            min = array[0][i];
+            for (int j = 0; j < rows; j++) {
+                if (array[j][i] < min)
+                    min = array[j][i];
+            }
+            sum += min;
+        }
+    }
+
+    return sum;
+}
+
+
+int main()
+{
+    srand((unsigned)time(NULL));
+    int Low = 1;
+    int High = 30;
+    int k;
+    int n;
+
+    cout << "k = "; cin >> k;
+    cout << "n = "; cin >> n;
+
+    int** array = new int* [k];
+    for (int i = 0; i < k; i++)
+        array[i] = new int[n];
+
+    Create(array, k, n, Low, High);
+    Print(array, k, n);
+
+    cout << "Sum : " << min_elements_sum(array, k, n) << endl;
+    for (int i = 0; i < k; i++)
+        delete[] array[i];
+    delete[] array;
+
+    return 0;
+}
---
+++ UnitTest7.2/UnitTest7.2.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../7.2/7.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest72
+{
+	TEST_CLASS(UnitTest72)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const int rows = 2;
+			const int cols = 2;
+			int** array = new int*[rows];
+			for (int j = 0; j < rows; j++) {
+				array[j] = new int[cols];
+			}
+			for (int i = 0; i < 2; i++)
+				for (int j = 0; j < 2; j++)
+					array[i][j] = i + j;
+			int s = min_elements_sum(array, rows, cols);
+			Assert::AreEqual(s, 0);
+		}
+	};
+}
